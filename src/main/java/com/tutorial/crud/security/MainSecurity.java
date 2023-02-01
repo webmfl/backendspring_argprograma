3311f94b19147d02f5de04a3681706435468d55f
Martin Lazo <66096850+webmfl@users.noreply.github.com>
2023-02-01 19:11:42 -0300

    ruta publica

--- src/main/java/com/tutorial/crud/security/MainSecurity.java
+++ src/main/java/com/tutorial/crud/security/MainSecurity.java
@@ -59,6 +59,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers("/exp/**").permitAll()
+                .antMatchers("/edu/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
